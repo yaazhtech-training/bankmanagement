a67dbc7d41886d2c44f2a7e3d10e8449df8b5755
PreethiRG <rgnkpreethi@gmail.com>
2025-10-06 11:25:21 +0530

    initial commit

---
+++ src/main/java/com/yaazhtech/bankmanagement/BankmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.yaazhtech.bankmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankmanagementApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/yaazhtech/bankmanagement/controller/AuthController.java
@@ -0,0 +1,4 @@
+package com.yaazhtech.bankmanagement.controller;
+
+public class AuthController {
+}
